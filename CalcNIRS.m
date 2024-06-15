78d00a265a601ef48abd37d6ce20df4026b4056c
Shani Sela <shanisela271@gmail.com>
2024-06-15 17:42:33 +0300

    changed main script

--- CalcNIRS.m
+++ CalcNIRS.m
@@ -1,11 +1,17 @@
 function [ dHbR , dHbO, fig ] = CalcNIRS(dataFile, SDS, tissueType, plotChannelIdx, extinctionCoefficientsFile, DPFperTissueFile, relDPFfile )
-%% Extract relevant data from the input dataFile
-wavelengths =dataFile.SD.Lambda; %Two wavelengths [nm]
-time = dataFile.t; %Time vector
-intensities = dataFile.d;
-%intensityLow = dataFile.d(:,1:20); %intensity levels at low WL
-%intensityHigh = dataFile.d(:,21:end); %intensity levels at high WL
-
+%% Verifying input validity
+% Check if dataFile is a .mat file and exists
+if ~ischar(dataFile) || ~exist(dataFile, 'file') || ~strcmp(dataFile(end-3:end), '.mat')
+    error('dataFile must be a valid .mat file.');
+end
+% Check if SDS is a numeric value
+if ~isnumeric(SDS) || ~isscalar(SDS)
+    error('SDS must be a numeric scalar.');
+end
+% Check if tissueType is a string
+if ~ischar(tissueType)
+    error('tissueType must be a string.');
+end
 %% Set default values 
 if nargin < 7 || isempty(relDPFfile)
     relDPFfile = '.\RelativeDPFCoefficients.csv';
@@ -20,37 +26,51 @@ if nargin < 4 || isempty(plotChannelIdx)
     plotChannelIdx = [];
 end
 
+%% Extract relevant data from the input dataFile
+% Load data from .mat file and validate its contents
+data = load(dataFile); 
+if ~isfield(data, 'SD') || ~isfield(data.SD, 'Lambda') || ~isfield(data, 't') || ~isfield(data, 'd')
+    error('dataFile must contain the fields: DS.Lambda, t, and d.');
+end
+if size(data.d, 2) ~= 40
+    error('Intensity data (d) must have 40 rows.');
+end
+
+wavelengths =data.SD.Lambda; %Two wavelengths [nm]
+time = data.t; %Time vector [sec]
+intensities = data.d; %Intensity measurements 
+
 %% Load extinction coefficients and DPF data
-extinctionCoefficients = readtable(extinctionCoefficientsFile);
+extinctionCoefficients = readtable(extinctionCoefficientsFile); %Absorption coefficient (epsilon) [L/mol*cm]
 DPFperTissue = readtable(DPFperTissueFile);
 relDPF = readtable(relDPFfile);
 
 
 %% Calculate DPF for the given tissue type and wavelengths
-DPF_807nm = DPFperTissue.DPF(strcmp(DPFperTissue.Tissue, tissueType));
-relDPF_factors = interp1(relDPF.wavelength, relDPF{:, 2:end}, wavelengths);
-DPF = DPF_807nm .* relDPF_factors;
+DPF807nm = DPFperTissue.DPF(strcmp(DPFperTissue.Tissue, tissueType)); %Finding the DPF at 807 for the required tissue type
+relDPFCoeff = interp1(relDPF.wavelength, relDPF.relDPFcoeff, wavelengths); %Find the relative DPF for the required wl
+DPF = DPF807nm .* relDPFCoeff; %calculate final DPF
 
 %% Calculate optical densities
-I0 = intensities(1, :);
+I0 = intensities(1,:);
 OD = log10(I0 ./ intensities);
 
 %% Calculate extinction coefficients for the given wavelengths
-epsilon_HbR = interp1(extinctionCoefficients.wavelength, extinctionCoefficients.HHb, wavelengths);
-epsilon_HbO = interp1(extinctionCoefficients.wavelength, extinctionCoefficients.HbO2, wavelengths);
+epsilonHbR = interp1(extinctionCoefficients.wavelength, extinctionCoefficients.HHb, wavelengths);
+epsilonHbO = interp1(extinctionCoefficients.wavelength, extinctionCoefficients.HbO2, wavelengths);
 
 %% Output
-dHbR = OD(:,1:20) ./ (epsilon_HbR(1) * DPF(1) * SDS);
-dHbO = OD(:,20:end) ./ (epsilon_HbO (2)* DPF(2) * SDS);
+dHbR = OD(:,1:20) ./ (epsilonHbR(1) * DPF(1) * SDS);
+dHbO = OD(:,21:end) ./ (epsilonHbO (2)* DPF(2) * SDS);
 
 %% Plot the specified channels
 figs = [];
-if ~isempty(plotChannelIdx) && isvector(plotChannelIdx) && all(plotChannelIdx >= 1 & plotChannelIdx <= 20 & rem(plotChannelIdx, 1) == 0)
+if ~isempty(plotChannelIdx) && isvector(plotChannelIdx) 
     for ch = plotChannelIdx
         fig = figure;
-        plot(time, dHbR(:, ch), 'b', 'LineWidth', 2);
+        plot(time, dHbR(:, ch), 'b');
         hold on;
-        plot(time, dHbO(:, ch), 'r', 'LineWidth', 2);
+        plot(time, dHbO(:, ch), 'r');
         hold off;
         title(sprintf('Channel %d', ch));
         xlabel('Time (s)');
@@ -62,6 +82,7 @@ elseif isempty(plotChannelIdx)
     disp('No channels specified for plotting.');
 else
     disp('Invalid input for plotChannelIdx. Please provide a vector with values in the range [1-20].');
+
 end
 
 
